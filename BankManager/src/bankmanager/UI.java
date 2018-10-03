d58d72c03ca84a6b175cb625a640d2e6c244bfcd
Oliver L. Enemærke <Einmarx@users.noreply.github.com>
2018-10-03 09:47:09 +0200

    Lmao fam

---
+++ BankManager/src/bankmanager/UI.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bankmanager;
+
+
+public class UI
+{
+    public static String(String)
+    {
+        System.out.println("Hello and welcome ");
+    }
+}
