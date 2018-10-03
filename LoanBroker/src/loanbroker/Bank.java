4031f5a0bd88617fda97a5145fce45b78a44f9cb
Rasmus Hemmingsen <rashem2@hotmail.com>
2018-10-03 10:39:57 +0200

    kk

--- LoanBroker/src/loanbroker/Bank.java
+++ LoanBroker/src/loanbroker/Bank.java
@@ -20,43 +20,4 @@ public class Bank
         this.response = response;
 
     }
-
-    public void start()
-    {
-        Scanner scan = new Scanner(System.in);
-        while (true)
-        {
-            String[] choices =
-            {
-                "New loan", "Get loan from cpr", "Quit"
-            };
-            UI.choice(choices);
-            UI.getInteger();
-            int aws = UI.getInteger();
-            if (aws == 2)
-            {
-                break;
-            } else if (aws == 1)
-            {
-                System.out.println("Please inter your cpr: ");
-                int cpr = UI.getInteger();
-                LoanQuote.setCpr(cpr);
-                
-               // LoanQuote.getLoan(saved);
-
-            } else if (aws == 0)
-            {
-                    
-            }
-        }
-
-    }
-
-    public void loanAPP()
-    {
-          System.out.println("Please enter your cpr: ");
-          
-    }
-    
-    
 }
